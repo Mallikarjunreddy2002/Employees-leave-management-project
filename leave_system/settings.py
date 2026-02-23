26cbd2e18551938f286a582c2776b67c0126bb23
Mallikarjun Laxman Narasaraddi <mnarasaraddi232002@gmail.com>
2026-02-23 15:00:45 +0530

    Production configuration update

--- leave_system/settings.py
+++ leave_system/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -31,6 +32,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'leave_system.urls'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 TEMPLATES = [
     {
